43ad2a3ba6a6cd872d638c80c16c62a9ff62a937
Tomas <volcan8@yahoo.es>
2020-12-02 23:48:38 -0600

    almacenamiento.py

--- almacenamiento.py
+++ almacenamiento.py
@@ -1,2 +1,33 @@
+
+
+As = [('archivo1', 10), ('archivo2', 5), ('archivo3', 3),
+              ('archivo4', 8), ('archivo5', 9), ('archivo6', 1)]
+
+D = 10
+M = []
+suma = 0
+
+
 def maximizar(As, D):
-    raise NotImplementedError()
+    resolver(As,D,suma, M)
+    
+    
+
+
+
+
+def resolver(As,D,suma, M):
+	for j in As:
+		if suma >= D:
+			return M
+		if j[1] <= D:
+			M += j
+			suma += j[1]
+
+			
+   
+maximizar(As,D)
+		
+
+	
+    		
